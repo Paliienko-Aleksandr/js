65ec7cf5576b3d75a453fa4ec1530b4fe66b1707
Paliienko-Aleksandr <89596420+Paliienko-Aleksandr@users.noreply.github.com>
2022-02-07 21:40:10 +0200

    changes

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
+function run() {
+  var a = 0;
 
-for (let i)
+  if (a == 1) {
+    return c;
+  }
+
+  for (let i = 1; i < 10; i++) {
+    console.log(i);
+  }
+
+  return (d = a);
+}
+
+run();
+
+
