a26eb0a86ce217fab9673f682a0079cb9fe63b76
Paliienko-Aleksandr <89596420+Paliienko-Aleksandr@users.noreply.github.com>
2022-03-17 15:44:31 +0200

    tests

---
+++ calculator.test.js
@@ -0,0 +1,22 @@
+import getSum {getSquaredArray, getOddNumbers} from './calculator.js'
+
+it ('should get square number', () => {
+  const result = getSquaredArray([1,2,3]);
+
+
+  expect(result).toEqual([1, 4, 9])
+});
+
+it ('should get odd number', () => {
+  const result = getOddNumbers([1,2,3,5]);
+
+
+  expect(result).toEqual([1, 2, 5])
+});
+
+it ('should get sum of numbers', () => {
+  const result = getOddNumbers([1,2,3,5]);
+
+
+  expect(result).toEqual([11])
+};
