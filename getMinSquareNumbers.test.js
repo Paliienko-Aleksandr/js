e16f9741d84fcea6b7153aea5256bdf708004646
Paliienko-Aleksandr <89596420+Paliienko-Aleksandr@users.noreply.github.com>
2022-03-20 12:21:17 +0200

    calc_tests

--- getMinSquareNumbers.test.js
+++
@@ -1,7 +0,0 @@
-import {getMinSquareNumbers} from './getMinSquareNumbers.js'
-
-it ('should return min square numbers from array', () =>{
-  const result = getMinSquareNumbers([1, 2, 3, 4]);
-
-  expect(result).toEqual(1);
-});
