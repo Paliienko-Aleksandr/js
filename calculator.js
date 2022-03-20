31706fa5e86eae2d1853cdb977516d8754c2e641
Paliienko-Aleksandr <89596420+Paliienko-Aleksandr@users.noreply.github.com>
2022-03-20 09:06:53 +0200

    final changes

---
+++ calculator.js
@@ -0,0 +1,12 @@
+export const getSquaredArray = arr =>
+
+  arr.map(num => (num * num))
+
+//  return getSquaredArray([1, 2, 3]))
+
+export const getOddNumbers = arr =>
+
+  arr.filter(num => num % 2 ===1)
+
+export default (a, b) => a + b
+
